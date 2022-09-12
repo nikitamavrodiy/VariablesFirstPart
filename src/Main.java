61ada6ed5878b8826d992c06eccba5ca9b16e139
nikitamavrodiy <mna1998@mail.ru>
2022-09-12 21:15:08 +0300

    Задача 5 - выполнил.

--- src/Main.java
+++ src/Main.java
@@ -27,5 +27,14 @@ public class Main {
         System.out.println(friend);
         friend = friend / 7;
         System.out.println(friend);
+
+        var frog = 3.5; //Задача 5
+        System.out.println(frog);
+        frog = frog * 10;
+        System.out.println(frog);
+        frog = frog / 3.5;
+        System.out.println(frog);
+        frog = frog + 4;
+        System.out.println(frog);
     }
 }
