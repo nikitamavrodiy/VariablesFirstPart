62936ca3e71b39087f74230432a17b8bfe469477
nikitamavrodiy <mna1998@mail.ru>
2022-09-12 20:54:02 +0300

    Задача 3 - выполнил.
    Для решения пришлось поменять значение типа int в переменной dog.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        var dog = 8;
+        var dog = 8.0;
         var cat = 3.6;
         var paper = 763789;
         System.out.println(dog);
@@ -14,5 +14,11 @@ public class Main {
         System.out.println(cat);
         System.out.println(paper);
 
+        dog = dog - 3.5;
+        cat = cat - 1.6;
+        paper = paper - 7639;
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
     }
 }
