0b8d2edc9dd207e40f43a8b08bd8900a6713a0e6
nikitamavrodiy <mna1998@mail.ru>
2022-09-12 21:10:50 +0300

    Задача 4 - выполнил.

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,31 @@
 public class Main {
     public static void main(String[] args) {
-        var dog = 8.0;
+        var dog = 8.0; //Задача 1
         var cat = 3.6;
         var paper = 763789;
         System.out.println(dog);
         System.out.println(cat);
         System.out.println(paper);
 
-        dog = dog + 4;
+        dog = dog + 4; //Задача 2
         cat = cat + 4;
         paper = paper + 4;
         System.out.println(dog);
         System.out.println(cat);
         System.out.println(paper);
 
-        dog = dog - 3.5;
+        dog = dog - 3.5; //Задача 3
         cat = cat - 1.6;
         paper = paper - 7639;
         System.out.println(dog);
         System.out.println(cat);
         System.out.println(paper);
+
+        var friend = 19; //Задача 4
+        System.out.println(friend);
+        friend = friend + 2;
+        System.out.println(friend);
+        friend = friend / 7;
+        System.out.println(friend);
     }
 }
